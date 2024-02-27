ac9f554c14865c5bfab0f94273c1caf4d43bb7f5
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2024-02-27 23:47:18 +0700

    Create a start menu scene 🖼️

---
+++ RoadToVKU/Assets/Scenes/Menu/ChucNangMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChucNangMenu : MonoBehaviour
+{
+    public void ChoiMoi()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Thoat()
+    {
+        Application.Quit();
+    }
+}
