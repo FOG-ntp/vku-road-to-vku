d2f5058064b815a5a8e3bf432932e12009e92a77
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2024-03-20 00:31:40 +0700

    feat: Flip player when click to right/left

--- RoadToVKU/Assets/Scripts/Player.cs
+++ RoadToVKU/Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     //Private Fields
     Rigidbody2D rb;
     float horizontalValue;
+    bool facingRight = true;
 
     void Awake()
     {
@@ -19,6 +20,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Lưu trữ giá trị ngang
         horizontalValue = Input.GetAxisRaw("Horizontal");
         //Debug.Log(horizontalValue);
     }
@@ -30,8 +32,24 @@ public class Player : MonoBehaviour
 
     void Move(float dir)
     {
-        float xVal = dir * speed * Time.deltaTime;
+        //Đặt giá trị của x bằng dir và speed
+        float xVal = dir * speed * 100 * Time.deltaTime;
+        // Tạo Vec2 cho vận tốc
         Vector2 targetVelocity = new Vector2(xVal,rb.velocity.y);
+        //Đặt vận tốc của người chơi
         rb.velocity = targetVelocity;
+
+        //Nếu look phải mà click trái (flip sang trái)
+        if(facingRight && dir < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            facingRight = false;
+        }
+        //Nếu look trái mà click phải (flip sang phải)
+        else if (!facingRight && dir > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            facingRight =true;
+        }
     }
 }
