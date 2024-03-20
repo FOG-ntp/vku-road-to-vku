e37a6dbcf1683f43b6bd1bb4c56aa9573e4daf0b
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2024-03-20 17:22:57 +0700

    feat: Create Simple Crouching

--- RoadToVKU/Assets/Scripts/Player.cs
+++ RoadToVKU/Assets/Scripts/Player.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //Private Fields
     Rigidbody2D rb;
     Animator animator;
+    public Collider2D standingCollider, crouchingCollider;
     public Transform groundCheckCollider;
+    public Transform overheadCheckCollider;
     public LayerMask groundLayer;
+    public Transform wallCheckCollider;
+    public LayerMask wallLayer;
 
     const float groundCheckRadius = 0.2f;
-    //const float overheadCheckRadius = 0.2f;
-    //const float wallCheckRadius = 0.2f;
+    const float overheadCheckRadius = 0.2f;
+    const float wallCheckRadius = 0.2f;
     [SerializeField] float speed = 2;
     [SerializeField] float jumpPower = 500;
-    //[SerializeField] float slideFactor = 0.2f;
-    //public int totalJumps;
-    //int availableJumps;
+    [SerializeField] float slideFactor = 0.2f;
+    public int totalJumps;
+    int availableJumps;
     float horizontalValue;
     float runSpeedModifier = 2f;
-    //float crouchSpeedModifier = 0.5f;
+    float crouchSpeedModifier = 0.5f;
 
-    
-    [SerializeField] bool isGrounded;
+    bool isGrounded = true;
     bool isRunning;
     bool facingRight = true;
-    bool jump;
     bool crouchPressed;
     bool multipleJump;
     bool coyoteJump;
@@ -35,101 +36,253 @@ public class Player : MonoBehaviour
 
     void Awake()
     {
+        availableJumps = totalJumps;
+
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Lưu trữ giá trị ngang
+        //if (CanMoveOrInteract() == false)
+        //    return;
+
+        //Store the horizontal value
         horizontalValue = Input.GetAxisRaw("Horizontal");
-        //Debug.Log(horizontalValue);
 
-        //Nếu LShift đc click thì cho phép isRunning
+        //If LShift is clicked enable isRunning
         if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
             isRunning = true;
-        }
+        //If LShift is released disable isRunning
         if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
             isRunning = false;
-        }
 
-        //Nếu press Jump button cho phép nhảy 
+        //If we press Jump button enable jump 
         if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }else if (Input.GetButtonUp("Jump"))
-        {
-            jump= false;
-        }
-            
+            Jump();
+
+        //If we press Crouch button enable crouch 
+        if (Input.GetButtonDown("Crouch"))
+            crouchPressed = true;
+        //Otherwise disable it
+        else if (Input.GetButtonUp("Crouch"))
+            crouchPressed = false;
+
+        //Set the yVelocity Value
+        animator.SetFloat("yVelocity", rb.velocity.y);
+
+        //Check if we are touching a wall to slide on it
+        WallCheck();
     }
 
     void FixedUpdate()
     {
         GroundCheck();
-        Move(horizontalValue, jump);
+        Move(horizontalValue, crouchPressed);
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(groundCheckCollider.position, groundCheckRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(overheadCheckCollider.position, overheadCheckRadius);
+    }
+
+    //bool CanMoveOrInteract()
+    //{
+    //    bool can = true;
+
+    //    if (FindObjectOfType<InteractionSystem>().isExamining)
+    //        can = false;
+    //    if (FindObjectOfType<InventorySystem>().isOpen)
+    //        can = false;
+    //    if (isDead)
+    //        can = false;
+
+    //    return can;
+    //}
 
     void GroundCheck()
     {
-        
+        bool wasGrounded = isGrounded;
         isGrounded = false;
-        //Kiểm tra xem GroundCheckObject có va chạm với cái khác không
-        //2D Colliders nằm trong "Ground" Layer
+        //Check if the GroundCheckObject is colliding with other
+        //2D Colliders that are in the "Ground" Layer
         //If yes (isGrounded true) else (isGrounded false)
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
         if (colliders.Length > 0)
         {
             isGrounded = true;
+            if (!wasGrounded)
+            {
+                availableJumps = totalJumps;
+                multipleJump = false;
+
+                //AudioManager.instance.PlaySFX("landing");
+            }
+
+            //Check if any of the colliders is moving platform
+            //Parent it to this transform
+            foreach (var c in colliders)
+            {
+                if (c.tag == "MovingPlatform")
+                    transform.parent = c.transform;
+            }
         }
-            
+        else
+        {
+            //Un-parent the transform
+            transform.parent = null;
+
+            if (wasGrounded)
+                StartCoroutine(CoyoteJumpDelay());
+        }
+
+        //As long as we are grounded the "Jump" bool
+        //in the animator is disabled
+        animator.SetBool("Jump", !isGrounded);
     }
 
-   
-
-    void Move(float dir,bool jumpFlag)
+    void WallCheck()
     {
-        //If the player is grounded and pressed space Jump
-        if (isGrounded && jumpFlag)
+        //If we are touching a wall
+        //and we are moving towards the wall
+        //and we are falling
+        //and we are not grounded
+        //Slide on the wall
+        if (Physics2D.OverlapCircle(wallCheckCollider.position, wallCheckRadius, wallLayer)
+            && Mathf.Abs(horizontalValue) > 0
+            && rb.velocity.y < 0
+            && !isGrounded)
         {
-            isGrounded = false;
-            jumpFlag = false;
-            //Add jump force
-            rb.AddForce(new Vector2(0f,jumpPower));
+            if (!isSliding)
+            {
+                availableJumps = totalJumps;
+                multipleJump = false;
+            }
+
+            Vector2 v = rb.velocity;
+            v.y = -slideFactor;
+            rb.velocity = v;
+            isSliding = true;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                availableJumps--;
+
+                rb.velocity = Vector2.up * jumpPower;
+                animator.SetBool("Jump", true);
+            }
+        }
+        else
+        {
+            isSliding = false;
+        }
+    }
+
+    #region Jump
+    IEnumerator CoyoteJumpDelay()
+    {
+        coyoteJump = true;
+        yield return new WaitForSeconds(0.2f);
+        coyoteJump = false;
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            multipleJump = true;
+            availableJumps--;
+
+            rb.velocity = Vector2.up * jumpPower;
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            if (coyoteJump)
+            {
+                multipleJump = true;
+                availableJumps--;
+
+                rb.velocity = Vector2.up * jumpPower;
+                animator.SetBool("Jump", true);
+            }
+
+            if (multipleJump && availableJumps > 0)
+            {
+                availableJumps--;
+
+                rb.velocity = Vector2.up * jumpPower;
+                animator.SetBool("Jump", true);
+            }
+        }
+    }
+    #endregion
+
+    void Move(float dir, bool crouchFlag)
+    {
+        #region Crouch
+
+        //If we are crouching and disabled crouching
+        //Check overhead for collision with Ground items
+        //If there are any, remain crouched, otherwise un-crouch
+        if (!crouchFlag)
+        {
+            if (Physics2D.OverlapCircle(overheadCheckCollider.position, overheadCheckRadius, groundLayer))
+                crouchFlag = true;
         }
 
-        #region Move & Run
-        //Đặt giá trị của x bằng dir và speed
-        float xVal = dir * speed * 100 * Time.fixedDeltaTime;
+        animator.SetBool("Crouch", crouchFlag);
+        standingCollider.enabled = !crouchFlag;
+        crouchingCollider.enabled = crouchFlag;
 
-        // Nếu chúng ta đang chạy nhiều lần với Running modifier (cao hơn)
+        #endregion
+
+        #region Move & Run
+        //Set value of x using dir and speed
+        float xVal = dir * speed * 100 * Time.fixedDeltaTime;
+        //If we are running mulitply with the running modifier (higher)
         if (isRunning)
             xVal *= runSpeedModifier;
-
-        // Tạo Vec2 cho vận tốc
-        Vector2 targetVelocity = new Vector2(xVal,rb.velocity.y);
-        //Đặt vận tốc của người chơi
+        //If we are running mulitply with the running modifier (higher)
+        if (crouchFlag)
+            xVal *= crouchSpeedModifier;
+        //Create Vec2 for the velocity
+        Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
+        //Set the player's velocity
         rb.velocity = targetVelocity;
 
-        //Nếu look phải mà click trái (flip sang trái)
-        if(facingRight && dir < 0)
+        //If looking right and clicked left (flip to the left)
+        if (facingRight && dir < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             facingRight = false;
         }
-        //Nếu look trái mà click phải (flip sang phải)
+        //If looking left and clicked right (flip to rhe right)
         else if (!facingRight && dir > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
-            facingRight =true;
+            facingRight = true;
         }
 
-        //(0 idle - 4 running)
-        // Đặt float xVelocity theo giá trị x của vận tốc RigidBody2D
+        //(0 idle , 4 walking , 8 running)
+        //Set the float xVelocity according to the x value 
+        //of the RigidBody2D velocity 
         animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
         #endregion
     }
+
+    //public void Die()
+    //{
+    //    isDead = true;
+    //    FindObjectOfType<LevelManager>().Restart();
+    //}
+
+    public void ResetPlayer()
+    {
+        isDead = false;
+    }
+
 }
