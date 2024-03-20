69f6a75a92e3f75c99d2dc79db99e359870cfc56
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2024-03-20 23:00:11 +0700

    feat: create 2D camera bounds

--- RoadToVKU/Assets/Scripts/CameraFollow.cs
+++ RoadToVKU/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,8 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     [Range(1, 10)]
     public float smoothFactor;
+    //[HideInInspector]
+    public Vector3 minValues, maxValue;
 
     private void FixedUpdate()
     {
@@ -16,8 +18,14 @@ public class CameraFollow : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
+        //Verify if the targetPosition is out of bound or not
+        //Limit it to the min and max values
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
 
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
         transform.position = smoothPosition;
     }
 }
