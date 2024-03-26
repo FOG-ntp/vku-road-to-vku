5e8ec382ae72c2c6910c446e93e0144e76f39b0a
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2024-03-26 13:35:47 +0700

    feat: Changing levels when collision to Level_Exit

---
+++ RoadToVKU/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string levelToLoad;
+
+    private bool playerInZone;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerInZone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetAxisRaw("Vertical") > 0 && playerInZone)
+        //{
+        //    Application.LoadLevel(levelToLoad);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.W) && playerInZone)
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            playerInZone = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            playerInZone = false;
+        }
+    }
+}
