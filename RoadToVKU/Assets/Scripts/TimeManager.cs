fbcf80c71849c727084b233933a78e1d4a27da23
ThanhPhong <nguyenthanhphong2912002@gmail.com>
2024-04-24 02:26:24 +0700

    feat; create level countdown timer

---
+++ RoadToVKU/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public float startingTime;
+    //public GameObject gameOverScreen;
+
+    private Text theText;
+    private PauseMenu thePauseMenu;
+    //private PlayerController player;
+    private float countingTime;
+    private HealthManager theHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        theText = GetComponent<Text>();
+        thePauseMenu = FindObjectOfType<PauseMenu>();
+        countingTime = startingTime;
+        theHealth = FindObjectOfType<HealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (thePauseMenu.isPaused)
+            return;
+
+        countingTime -= Time.deltaTime;
+
+        if (countingTime <= 0)
+        {
+            theHealth.KillPlayer();
+        }
+
+        theText.text = "" + Mathf.Round(countingTime);
+    }
+
+    public void ResetTime()
+    {
+        countingTime = startingTime;
+    }
+}
